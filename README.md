9b5074382c1b4b527ef0607ba84b863a6ba9d668
trust@kalimdor <trust@kalimdor>
2025-01-02 16:26:57 +0100

    add tool

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # Radagast
-Web pentest toolkit
+
+## Introduction
+radagast is an open source penetration testing tool, able to perform a web fuzzer attack against http services. 
+
+## Installation
+
+
+### Linux distro
+```bash
+pip install -r requirements.txt
+ ```
+
+### From virtual env
+```bash
+python3 -m venv sshscan
+source sshscan/bin/activate
+pip install -r requirements.txt
+
+ ```
+
+## Usage
+```bash
+
+
+usage: radagast.py [-h] [--url URL] [--wordlist WORDLIST] [--ic IC] [--timeout TIMEOUT]
+
+Tiny web fuzzer
+
+options:
+  -h, --help            show this help message and exit
+  --url URL, -u URL     Target URL
+  --wordlist WORDLIST, -w WORDLIST
+                        Specify a wordlist file
+  --ic IC               Include response code ex: 200,301,500
+  --timeout TIMEOUT, -t TIMEOUT
+                        Request timeout
+
+
+```
+### Default wordlist fuzzing
+```bash
+python radagast.py --url http://localhost:8000/vulnerabilities/FUZZ --wordlist wordlist.txt
+
+
+```
+
+### Wordlist fuzzing filter by response code
+```bash
+python radagast.py --url http://localhost:8000/vulnerabilities/FUZZ --wordlist wordlist.txt --ic 200 --timeout 1
+
+
+```
+
+
+
+### Wordlist fuzzing specify timeout
+```bash
+python radagast.py --url http://localhost:8000/vulnerabilities/FUZZ --timeout 1
+
+
+```
---
+++ radagast.py
@@ -0,0 +1,55 @@
+import argparse
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import signal
+import sys
+import time
+from threading import Thread
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+web_scan = None
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    global EXIT
+    EXIT=True
+    web_scan.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Tiny web fuzzer")
+    parser.add_argument("--url", "-u", type=str, default=None, help="Target URL")
+    parser.add_argument("--wordlist", "-w", type=str, default=None, help="Specify a wordlist file")
+    parser.add_argument("--ic",type=str, default=None, help="Include response code ex: 200,301,500")
+    parser.add_argument("--timeout","-t",type=int, default=5, help="Request timeout")
+
+    args = parser.parse_args()
+    
+        
+    if args.wordlist:
+        include_code_list = []
+        if args.ic:
+            for code in args.ic.split(","):
+                include_code_list.append(int(code.strip()))
+        if args.url:
+            file = open(args.wordlist, "r", encoding="utf-8")
+            while True:
+                word=file.readline()
+                if not word:
+                    break
+                attack_url = args.url.replace("FUZZ", word.strip())
+                try:
+                    response = requests.get(attack_url, timeout=args.timeout)
+                    if len(include_code_list)==0 or (len(include_code_list)>0 and response.status_code in include_code_list):
+                            print(f"{attack_url}\t[Code: {response.status_code}]\t[Size: {len(response.text)}]")
+                except Exception as ex:
+                    print(f"Error: {attack_url}\t{str(ex)}")
+            file.close()
+    else:
+        print("Error: you need to specify a wordlist file")
+        
+
+    
